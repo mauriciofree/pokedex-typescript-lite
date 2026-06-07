d9840b2d1781f7d669818ac07905957202b38549
mauriciofree <mauricio@freeline.inf.br>
2026-06-07 10:29:37 -0300

    feat: criar funcao adicionar no PokemonResumo

--- src/models/CatalogoPokemon.ts
+++ src/models/CatalogoPokemon.ts
@@ -2,6 +2,22 @@ import type { PokemonResumo } from "./PokemonResumo";
 
 export class CatalogoPokemon {
 
-  private pokemons: PokemonResumo[] = [];   
+    private pokemons: PokemonResumo[] = [];   
 
+    
+    adicionar(pokemon: PokemonResumo): void {
+
+        const jaExiste = this.pokemons.some(
+        item => item.id === pokemon.id
+        );
+
+        if (jaExiste) {
+        console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
+        return;
+        }
+
+        this.pokemons.push(pokemon);
+
+        console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`);
+    } 
 }
