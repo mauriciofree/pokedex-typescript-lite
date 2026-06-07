650cab331f5b44c12fbac62b4af4dd08278f91be
mauriciofree <mauricio@freeline.inf.br>
2026-06-07 10:50:24 -0300

    feat: criar funcao de listar na classe CatalogoPokemon

--- src/models/CatalogoPokemon.ts
+++ src/models/CatalogoPokemon.ts
@@ -4,20 +4,37 @@ export class CatalogoPokemon {
 
     private pokemons: PokemonResumo[] = [];   
 
-    
+
     adicionar(pokemon: PokemonResumo): void {
 
         const jaExiste = this.pokemons.some(
-        item => item.id === pokemon.id
+            item => item.id === pokemon.id
         );
 
         if (jaExiste) {
-        console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
-        return;
+            console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
+            return;
         }
 
         this.pokemons.push(pokemon);
 
         console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`);
     } 
+
+    listar(): void {
+
+        if (this.pokemons.length === 0) {
+            console.log("[AVISO] Catálogo vazio.");
+            return;
+        }
+
+        this.pokemons.forEach(pokemon => {
+
+        console.log(
+            `#${pokemon.id} - ${pokemon.nome} | Tipos: ${pokemon.tipos.join(", ")} | Altura: ${pokemon.altura} | Peso: ${pokemon.peso}`
+        );
+
+        });
+
+    }
 }
