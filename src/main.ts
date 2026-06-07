702ef4027b8c44e59920321dec3ce78e60f5cb46
mauriciofree <mauricio@freeline.inf.br>
2026-06-07 09:57:06 -0300

    feat: Execução da rotina de pokemon inexistente

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { buscarPokemon } from "./services/PokeAPI";
 
 async function main() {
   const pokemon = await buscarPokemon("pikachu");
-
-  console.log(pokemon);
+  if (pokemon){
+    console.log(pokemon);
+  }
+  const inexistente  = await buscarPokemon("pokemon-inexistente");
+  if (inexistente){
+    console.log(inexistente);
+  }
 }
 
 main();
